df50344075159047ba43bc8cfdb0d9a306b8e613
Mirza Leka <mirza_376@hotmail.com>
2019-05-16 23:42:18 +0200

    Init commit. Created todos controller

---
+++ TodoApp/Controllers/TodoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class TodoController : ApiController
+    {
+
+		List<string> todos = new List<string>();
+
+
+        public IEnumerable<string> GetAllTodos()
+        {
+            return todos;
+        }
+
+		public string GetOneTodo(int id)
+		{
+			return todos[id];
+		} 
+
+		public IHttpActionResult AddTodo([FromBody] string text)
+		{
+			todos.Add(text);
+			return Ok();
+		}
+
+		public IHttpActionResult UpdateTodo(int id, [FromBody] string newValue)
+		{
+			todos[id] = newValue;
+			return Ok();
+		}
+
+		public IHttpActionResult DeleteTodo(int id)
+		{
+			todos.RemoveAt(id);
+			return Ok();
+		}
+		
+    }
+}
